dc4997b3029299401a8a7900909d0a3cb856e54f
Dominic Kuang <finaiized@gmail.com>
2017-06-12 11:50:14 -0700

    Code cleanup

--- aispace/cspviewer.py
+++ aispace/cspviewer.py
@@ -1,37 +1,38 @@
-from ipywidgets import DOMWidget, CallbackDispatcher, register, Output
-from traitlets import Unicode, observe, Integer
-from aipython.utilities import cspToJson, Displayable
-from functools import partial
 import importlib
+import multiprocessing
+import threading
+from functools import partial
 from time import sleep
+
 import ipywidgets as widgets
 from IPython.display import display
-import threading
-import multiprocessing
+from ipywidgets import CallbackDispatcher, DOMWidget, Output, register
+from traitlets import Integer, Unicode, observe
+
+from aipython.utilities import Displayable, cspToJson
 
 @register('aispace.CSPViewer')
 class CSPViewer(DOMWidget):
-    """"""
+    """Visualize and interact with a CSP."""
     _view_name = Unicode('HelloView').tag(sync=True)
     _model_name = Unicode('HelloModel').tag(sync=True)
     _view_module = Unicode('aispace').tag(sync=True)
     _model_module = Unicode('aispace').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
-    value = Unicode('Hello World!!!!').tag(sync=True)
+    
+    # value = Unicode('Hello World!!!!').tag(sync=True)
     jsonRepr = Unicode('{}').tag(sync=True)
-    process_id = Integer(0).tag(sync=True)
+    # process_id = Integer(0).tag(sync=True)
 
     def __init__(self, csp):
-
         super(CSPViewer, self).__init__()
         self.on_msg(self._handle_custom_msgs)
         self.jsonRepr = cspToJson(csp)
-        self.desired_level = 4
+        self._desired_level = 4
         self.sleep_time = 0.2
 
-        # If Con_solver is not defined at the time of creation,
-        # import the existing one
+        # If Con_solver is not defined at the time of creation, import the existing one
         try:
             module = importlib.import_module('__main__')
             Con_solver = getattr(module, 'Con_solver')
@@ -39,35 +40,34 @@ class CSPViewer(DOMWidget):
             module = importlib.import_module('aipython.cspConsistency')
             Con_solver = getattr(module, 'Con_solver')
 
-        self.con_solver = Con_solver(csp)
-        self.con_solver.display = self.display
-        self.event = threading.Event()
+        self._con_solver = Con_solver(csp)
+        self._con_solver.display = self.display
+        self._block_for_user_input = threading.Event()
 
         def arc_select(to_do):
-            if self.desired_level == 1:
+            if self._desired_level == 1:
                 return to_do.pop()
 
-            self.event.wait()
+            self._block_for_user_input.wait()
             
-            if self.userSelected:
-                to_do.discard(self.last_selected_arc)
-                return self.last_selected_arc
+            if self._user_selected_arc:
+                to_do.discard(self._selected_arc)
+                return self._selected_arc
             else:
                 # User did not select. Return random arc
                 return to_do.pop()
 
-        self.con_solver.wait_for_arc_selection = arc_select
+        self._con_solver.wait_for_arc_selection = arc_select
 
         def bootstrap():
-            self.process_id = threading.current_thread().ident
-            self.con_solver.make_arc_consistent()
-
+            # self.process_id = threading.current_thread().ident
+            self._con_solver.make_arc_consistent()
 
         self.thread = threading.Thread(target=bootstrap)
         self.thread.start()
 
-        self.last_selected_arc = None
-        self.userSelected = False
+        self._selected_arc = None
+        self._user_selected_arc = False
 
         """yield example
         self.gen = self.con_solver.make_arc_consistent()
@@ -78,42 +78,39 @@ class CSPViewer(DOMWidget):
         display(self.a)
         """
 
-        def d(btn):
-            self.userSelected = False
-            self.desired_level = 2
-            self.event.set()
-            self.event.clear()
+        def step(btn):
+            self._user_selected_arc = False
+            self._desired_level = 2
+            self._block_for_user_input.set()
+            self._block_for_user_input.clear()
 
-        def a(btn):
-            self.userSelected = False
-            self.desired_level = 4
-            self.event.set()
-            self.event.clear()
+        def fine_step(btn):
+            self._user_selected_arc = False
+            self._desired_level = 4
+            self._block_for_user_input.set()
+            self._block_for_user_input.clear()
 
-        def e(btn):
-            self.userSelected = False
-            self.desired_level = 1
-            self.event.set()
-            self.event.clear()
+        def auto_arc(btn):
+            self._user_selected_arc = False
+            self._desired_level = 1
+            self._block_for_user_input.set()
+            self._block_for_user_input.clear()
 
-        b1 = widgets.Button(description='Fine Step')
-        b2 = widgets.Button(description='Step')
-        b3 = widgets.Button(description='AutoSolve')
-        b4 = widgets.Button(description='Reset')
+        fine_step_btn = widgets.Button(description='Fine Step')
+        fine_step_btn.on_click(fine_step)
+        step_btn = widgets.Button(description='Step')
+        step_btn.on_click(step)
+        auto_arc_btn = widgets.Button(description='Auto Arc Consistency')
+        auto_arc_btn.on_click(auto_arc)
+        reset_btn = widgets.Button(description='Reset')
         
-        b2.on_click(d)
-        b1.on_click(a)
-        b3.on_click(e)
-        v = widgets.HBox([b1, b2, b3])
-        display(v)
-        self.send({'action': 'rerender'})
-        # self.con_solver.display = self.display
+        display(widgets.HBox([fine_step_btn, step_btn, auto_arc_btn]))
 
     def _handle_custom_msgs(self, _, content, buffers=None):
         if content.get('event', '') == 'constraint:click':
             varChar = content.get('varId')
-            const = self.con_solver.csp.constraints[content.get('constId')]
-            self.desired_level = 2
+            const = self._con_solver.csp.constraints[content.get('constId')]
+            self._desired_level = 2
             # Search through to_do to find the one to pop
             '''if (varChar, const) in self.to_dos:
                 print("FOUND!")
@@ -123,11 +120,10 @@ class CSPViewer(DOMWidget):
                 except StopIteration:
                     pass'''
 
-            self.last_selected_arc = (varChar, const)
-            self.userSelected = True
-            self.event.set()
-            self.event.clear()
-            #self.con_solver.make_arc_consistent({(varChar, const)})
+            self._selected_arc = (varChar, const)
+            self._user_selected_arc = True
+            self._block_for_user_input.set()
+            self._block_for_user_input.clear()
     
     def display(self, level, *args, **kwargs):
         """print the arguments if level is less than or equal to the
@@ -152,8 +148,8 @@ class CSPViewer(DOMWidget):
             self.send({'action': 'highlightArc', 'varName': varName,
                 'consName': consName.__repr__(), 'style': 'bold',
                 'colour': 'na'})
-            for i in range(len(self.con_solver.csp.constraints)):
-                if self.con_solver.csp.constraints[i] == consName:
+            for i in range(len(self._con_solver.csp.constraints)):
+                if self._con_solver.csp.constraints[i] == consName:
                     self.send({'action': 'highlightArc', 'varName': varName,
                 'consName': consName.__repr__(), 'style': 'bold',
                 'colour': 'na'})
@@ -195,8 +191,8 @@ class CSPViewer(DOMWidget):
         text = ' '.join(map(str, args))
         self.send({'action': 'output', 'result': text})
 
-        if level <= self.desired_level:
+        if level <= self._desired_level:
             if shouldWait:
-                self.event.wait()
+                self._block_for_user_input.wait()
         else:
             sleep(self.sleep_time)
