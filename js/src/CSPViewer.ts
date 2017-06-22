706bd468d0429db433ec3ed81460dac48ef5661b
Dominic Kuang <finaiized@gmail.com>
2017-06-22 15:54:56 -0700

    Allow directly creating a `Con_solver` and queuing methods on it
    One problem we found is that there is fundamentally a disconnect between
    the wrapper Jupyter widget and `Con_solver`. There is no explicit
    indication when creating a `CSPViewer` that it creates a `Con_solver`.
    Furthermore, there is also a disconnect between which function you are
    calling whenever anything happens.
    
    The solution to the former issue is directly creating a `Con_solver` and
    passing it directly to the `CSPViewer`: that establishes the connection,
    as desired. Another solution is to make `Con_solver ` a Jupyter widget,
    such that it can be created and displayed directly. That is the approach
    taken here. One drawback is that all code now must be hidden inside
    `Displayable` to avoid polluting `Con_solver`'s user-facing code; that
    makes `Displayable` unique to `Con_solver` and not reusable. Perhaps we
    could remedy that by importing a different `Displayable` for each class
    that can be displayed.
    
    The latter issue is a bit more thorny. If we naively call the functions
    directly, due to the presence of blocking functions (to wait for user
    input, for example), then the function will not complete and nothing is
    ever displayed on the screen! Calling it after also causes it's own set
    of problems (and also very annoying - you need a separate cell to do
    it!). The approach here is to introduce a decorator that queues the
    function call, and executes it once displayed. Further calls of that
    method (e.g. recursively) do not queue the method. What's particularly
    great about being able to do this is that it partially solves the problem of
     what happens when a new method is added by the user (for example, adding
    `solve_all` as part of an assignment). How do they call that function?
    Previously, there was no direct way to do so. We still need to consider
    how to allow the user to add new buttons or further control the
    visualization, though.
    
    This allows us to call `Con_solver()` and
    `Con_solver().make_arc_consistent()` and have things display properly.
    As always, the tradeoff is more code presented to the user, and perhaps
    unintutitive behaviour (regarding queued function calls).

--- js/src/CSPViewer.ts
+++ js/src/CSPViewer.ts
@@ -7,15 +7,14 @@ import CSPGraphInteractor from "./CSPGraphInteractor";
 import { IGraphJSON } from "./Graph";
 
 import * as template from "./cspviewer.template.html";
-import { IEvent, isHighlightArcEvent, isOutputEvent, isRerenderEvent, isSetDomainEvent } from "./CSPViewerEvents";
+import { IEvent, isHighlightArcEvent, isOutputEvent, isRerenderEvent, isSetDomainEvent, isBeginFuncEvent } from "./CSPViewerEvents";
 import CSPViewerModel from "./CSPViewerModel";
 
 export default class CSPViewer extends widgets.DOMWidgetView {
     private static readonly ARC_CLICK = "arc:click";
     private static readonly FINE_STEP_CLICK = "fine-step:click";
     private static readonly STEP_CLICK = "step:click";
-    private static readonly AUTO_AC_CLICK = "auto-ac:click";
-    private static readonly AUTO_SOLVE_CLICK = "auto-solve:click";
+    private static readonly AUTO_STEP_CLICK = "auto-step:click";
     private static readonly BACKTRACK_CLICK = "backtrack:click";
 
     public model: CSPViewerModel;
@@ -23,6 +22,10 @@ export default class CSPViewer extends widgets.DOMWidgetView {
 
     public initialize(opts: any) {
         super.initialize(opts);
+
+        if (this.model.initial_render) {
+            this.send({ event: "initial_render" });
+        }
 
         this.visualization = new CSPGraphInteractor();
         this.visualization.onArcClicked = (varId, constId) => {
@@ -43,15 +46,15 @@ export default class CSPViewer extends widgets.DOMWidgetView {
                 this.draw();
                 this.model.trigger("msg:custom",
                     { action: "highlightArc", arcId: null, style: "normal", colour: "blue" });
+            } else if (isBeginFuncEvent(event)) {
+                this.$("#controls").show();
             }
         });
     }
 
     public events(): Backbone.EventsHash {
         return {
-            "click #auto-ac": (e) => this.send({ event: CSPViewer.AUTO_AC_CLICK }),
-            "click #auto-solve": (e) => this.send({ event: CSPViewer.AUTO_SOLVE_CLICK }),
-            "click #backtrack": (e) => this.send({ event: CSPViewer.BACKTRACK_CLICK }),
+            "click #auto-step": (e) => this.send({ event: CSPViewer.AUTO_STEP_CLICK }),
             "click #fine-step": (e) => this.send({ event: CSPViewer.FINE_STEP_CLICK }),
             "click #step": (e) => this.send({ event: CSPViewer.STEP_CLICK }),
 
