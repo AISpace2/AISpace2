b94eef7340fb20bf29f473dc46156a7a3be3f212
Dominic Kuang <finaiized@gmail.com>
2017-08-03 16:49:23 -0700

    Clarify when you need to generate DLLs

--- js/vendor/vendor.js
+++ js/vendor/vendor.js
@@ -6,6 +6,8 @@
  * into the main chunk. Any imported library not listed here
  * will be bundled as normal - you do not *have* to list libraries here.
  * 
+ * It is currently automatically run for you if you run `npm run dev` or `npm run build:dev`.
+ * 
  * This file is NOT used for production.
  */
 
