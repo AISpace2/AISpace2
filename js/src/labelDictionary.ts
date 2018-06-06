a4230387eed0bc94c138edb9c39129773e0189f8
Richard Chiang <shadowlairs@gmail.com>
2018-06-06 12:34:50 -0700

    fix dictionary

--- js/src/labelDictionary.ts
+++ js/src/labelDictionary.ts
@@ -1,5 +1,5 @@
-export const cspLabelText = ["not arc consistent", "arc consistent"];
-export const cspLabelColor = ["#0000FF", "#008300"];
+export const cspLabelText = ["todo", "consistent", "inconsistent"];
+export const cspLabelColor = ["#0000FF", "#008300", "#ff0000"];
 
 export const searchLabelText = ["neighbors found", "expanding"];
 export const searchLabelColor = ["#4256f4", "#ff0000"];
