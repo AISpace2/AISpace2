74ad5d82c5da7418cf50f1d406e84eeb24b71eec
Richard Chiang <shadowlairs@gmail.com>
2018-06-07 15:03:23 -0700

    update label text and color

--- js/src/labelDictionary.ts
+++ js/src/labelDictionary.ts
@@ -1,5 +1,14 @@
-export const cspLabelText = ["todo", "consistent", "inconsistent"];
-export const cspLabelColor = ["#0000FF", "#008300", "#ff0000"];
+const COLOR = {
+  BLUE: "#0000FF",
+  GREEN: "#008300",
+  RED: "#ff0000",
+  GREY: "#A8A8A8",
+  YELLOW: "#FFD700",
+  PURPLE: "#dc7cf4"
+};
 
-export const searchLabelText = ["neighbors found", "expanding"];
-export const searchLabelColor = ["#4256f4", "#ff0000"];
+export const cspLabelText = ["todo arc", "consistent arc", "inconsistent arc"];
+export const cspLabelColor = [COLOR.BLUE, COLOR.GREEN, COLOR.RED];
+
+export const searchLabelText = ["nodes along path", "action path", "neighbor nodes", "highlight", "origin" , "goal"];
+export const searchLabelColor = [COLOR.GREY, COLOR.RED, COLOR.BLUE, COLOR.GREEN, COLOR.PURPLE, COLOR.YELLOW];
