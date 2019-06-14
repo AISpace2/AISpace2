551a7509a9c2cfc3253994d32dd9ef763130adf1
c2e1b <chenliang.zhou@qq.com>
2019-06-13 17:43:45 -0700

    Revert "Capitalize folders"

--- aipythonV/README.md
+++
@@ -1,1 +0,0 @@
-This folder contains the [accompanying Python code](http://aipython.org) from the [book](http://artint.info). They are used as-is by some of the notebooks.
--- aipythonV/probBayesianNetwork.py
+++
@@ -1,6 +0,0 @@
-from aispace2.jupyter.bayes import Displayable
-class Bayesian_Network(Displayable):
-    def __init__(self, problem):
-        self.problem = problem
-        self.decimal_place = 2
-        super().__init__()
