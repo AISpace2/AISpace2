747639607014beef553aeae6a8152ba4ee47a27d
Chenliang Zhou <39361141+Chenliang-Zhou@users.noreply.github.com>
2019-06-16 09:14:20 -0700

    Update BaseEdge.vue

--- js/src/components/BaseEdge.vue
+++ js/src/components/BaseEdge.vue
@@ -203,11 +203,11 @@
       text-shadow: -1px -1px 0 black,
                     0   -1px 0 black,
                     1px -1px 0 black,
-                   1px  0   0 black,
-                   1px  1px 0 black,
-                   0    1px 0 black,
-                    -1px  1px 0 black,
-                    -1px  0   0 black;
+                    1px  0   0 black,
+                    1px  1px 0 black,
+                    0    1px 0 black,
+                   -1px  1px 0 black,
+                   -1px  0   0 black;
      
     }
 </style>
