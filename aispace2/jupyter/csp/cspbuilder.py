cbbe04e3d23d12acceb94e9ca689cdf581ebb74e
Chenliang Zhou <39361141+Chenliang-Zhou@users.noreply.github.com>
2019-06-25 15:08:46 -0700

    Update cspbuilder.py

--- aispace2/jupyter/csp/cspbuilder.py
+++ aispace2/jupyter/csp/cspbuilder.py
@@ -45,9 +45,7 @@ class CSPBuilder(DOMWidget):
 
     def py_code(self):
         """Converts the CSP represented by this builder into Python code."""
-        return print(csp_to_python_code(self.csp()))
-#         The following code works only in notebook.
-#         self.send({
-#             'action': 'python-code',
-#             'code': csp_to_python_code(self.csp())
-#         })
+         self.send({
+             'action': 'python-code',
+             'code': csp_to_python_code(self.csp())
+         })
