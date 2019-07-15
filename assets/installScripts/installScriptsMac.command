3fd534ffc5bf071b3472d2eec1d9f66ced8cc69a
Chenliang-Zhou <chenliang.zhou@qq.com>
2019-07-15 13:34:12 -0700

    improve install scripts
    improve install scripts

--- assets/installScripts/installScriptsMac.command
+++ assets/installScripts/installScriptsMac.command
@@ -1,12 +1,13 @@
 cd "$(dirname "$0")"
-chmod u+x jupyterlab.command
-cp jupyterlab.command ../jupyterlab.command
-cd ..
 cd AISpace2
+echo Make sure you have installed Anaconda, git, Node.js and npm.
+echo Installing jupyterlab...
 conda install -c conda-forge jupyterlab
+echo Installing ipywidgets...
 conda install -c conda-forge ipywidgets # Enter 'y' when prompted to install
-pip install -e .
 jupyter labextension install @jupyter-widgets/jupyterlab-manager@0.38
+echo install AISpace2 library...
+pip install -e .
 cd js
 npm install
 npm run update-lab-extension
--- assets/installScripts/installScriptsWindows.bat
+++ assets/installScripts/installScriptsWindows.bat
@@ -2,15 +2,14 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
 title Installation of AISpace2
-echo Make sure you have Installed Anaconda, git, nodejs and npm and add them to PATH.
-pause
+echo Make sure you have installed Anaconda, git, Node.js and npm.
 echo Installing jupyterlab...
 conda install -c conda-forge jupyterlab
 echo Installing ipywidgets...
 conda install -c conda-forge ipywidgets
 echo install labextension...
 jupyter labextension install @jupyter-widgets/jupyterlab-manager@0.38
-echo install AISpace2 library
+echo install AISpace2 library...
 pip install -e .
 pip install -r requirements-dev.txt
 cd js
