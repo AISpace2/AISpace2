6dbcf9eb330386b8148ffdd9f8f0a0161eac9a52
Chenliang-Zhou <chenliang.zhou@qq.com>
2019-07-15 18:41:25 -0700

    update install command

--- installScripts/installScriptsWindows.bat
+++ installScripts/installScriptsWindows.bat
@@ -4,17 +4,17 @@ echo Make sure you have installed Anaconda, git, Node.js and npm.
 pause
 cd ..
 echo Installing jupyterlab ...
-conda install -c conda-forge jupyterlab
+call conda install -c conda-forge jupyterlab
 echo Installing ipywidgets. Enter 'y' when prompted to install ...
-conda install -c conda-forge ipywidgets
+call conda install -c conda-forge ipywidgets
 echo Installing labextension ...
 jupyter labextension install @jupyter-widgets/jupyterlab-manager@0.38
 echo Installing AISpace2 library ...
 pip install -e .
 pip install -r requirements-dev.txt
 cd js
-npm install
-npm run update-lab-extension
+call npm install
+call npm run update-lab-extension
 jupyter labextension install
 cd ..
 jupyter lab
