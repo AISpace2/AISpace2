03e54c88e83b077f20e8f36b90477647a428279c
Chenliang-Zhou <chenliang.zhou@qq.com>
2019-07-17 19:41:54 -0700

    down load from pip not anaconda

--- installScripts/installScriptsWindows.bat
+++ installScripts/installScriptsWindows.bat
@@ -1,12 +1,12 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
-echo Make sure you have installed Anaconda, git, Node.js and npm.
+echo Make sure you have installed Anaconda, git, Node.js.
 pause
 cd ..
 echo Installing jupyterlab ...
-call conda install -c conda-forge jupyterlab
-echo Installing ipywidgets. Enter 'y' when prompted to install ...
-call conda install -c conda-forge ipywidgets
+call pip install --upgrade jupyterlab
+echo Installing ipywidgets ...
+call pip install --upgrade ipywidgets
 echo Installing labextension ...
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
 echo Installing AISpace2 library ...
@@ -14,6 +14,7 @@ pip install -e .
 pip install -r requirements-dev.txt
 cd js
 call npm install
+echo Building AISpace2 frontend ...
 call npm run update-lab-extension
 jupyter labextension install
 cd ..
--- trash/installScriptsLinux.sh
+++ trash/installScriptsLinux.sh
@@ -1,10 +1,11 @@
 dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd $dir
+read -n1 -r -p "Make sure you have installed Anaconda, git, Node.js.`echo $'\nPress any key to continue ...'`"
 cd ..
 echo Installing jupyterlab ...
-conda install -c conda-forge jupyterlab
-echo Installing ipywidgets. Enter 'y' when prompted to install ...
-conda install -c conda-forge ipywidgets
+pip install --upgrade jupyterlab
+echo Installing ipywidgets ...
+pip install --upgrade ipywidgets
 echo Installing labextension ...
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
 echo Installing AISpace2 library ...
@@ -12,6 +13,7 @@ pip install -e .
 pip install -r requirements-dev.txt
 cd js
 npm install
+echo Building AISpace2 frontend ...
 npm run update-lab-extension
 jupyter labextension install
 cd ..
