155571c5f8ce061665b12606fc4239e3bb106019
Dylan Yung <YungDexiong@hotmail.com>
2019-08-03 18:21:35 -0700

    added comment to webpack.config

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,5 +1,6 @@
 // Entry point webpack config that delegates to different environments depending on the --env passed in.
 module.exports = function(env) {
+  // env is set under webpack.labExtension.js under "mode." If you're a developer ensure it is set to "development" if you're a user of AISpace2 just set to "production"
   process.env.NODE_ENV = env;
   return require(`./webpack.${env}.js`);
 };
