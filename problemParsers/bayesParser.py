1528b952bb459b228776d871fd80cafa25446771
zijia zhang <zijia.zhang99@gmail.com>
2019-08-22 16:34:49 -0700

    Make Parsers as notebooks.

--- problemParsers/bayesParser.py
+++ problemParsers/bayesParser.py
@@ -22,59 +22,62 @@ def findallcontain(elem,tag):
             temp.append(child.text);
     return temp
 
+def loadFromXML(path):
+    try:
+        tree = ET.parse(path)
+    except:
+        print('Error Reading Files')
+        return;
+    root = tree.getroot()
 
-tree = ET.parse('/Users/zijiazhang/belief.xml')
-root = tree.getroot()
+    for ch in root:
+        if 'NETWORK' in ch.tag:
+            prob = ch
+    names = set()
+    domain = {}
+    probTable = {}
+    givenTable = {}
+    for element in prob:
+        if 'NAME' in element.tag:    
+            probname = findcontain(prob,'NAME').text.replace(' ', '_')
+        elif 'VARIABLE' in element.tag:
+            temp = findallcontain(element,'OUTCOME')
+            name = findcontain(element,'NAME').text
+            names.add(name)
+            domain[name] = temp
+        elif 'DEFINITION' in element.tag:
+            name = findcontain(element,'FOR').text
+            given = findallcontain(element,'GIVEN')
+            probs = findcontain(element,'TABLE').text
+            probTable[name] = probs.split(' ')
+            givenTable[name] = given
 
-for ch in root:
-    if 'NETWORK' in ch.tag:
-        prob = ch
-names = set()
-domain = {}
-probTable = {}
-givenTable = {}       
-for element in prob:
-    if 'NAME' in element.tag:    
-        probname = findcontain(prob,'NAME').text.replace(' ', '_')
-    elif 'VARIABLE' in element.tag:
-        temp = findallcontain(element,'OUTCOME')
-        name = findcontain(element,'NAME').text
-        names.add(name)
-        domain[name] = temp
-    elif 'DEFINITION' in element.tag:
-        name = findcontain(element,'FOR').text
-        given = findallcontain(element,'GIVEN')
-        probs = findcontain(element,'TABLE').text
-        probTable[name] = probs.split(' ')
-        givenTable[name] = given
+    domainNames = []
+    domains = []
+    for i in names:
+        t = (i,domain[i])
+        temp = i + " = Variable" +  str(t)
+        domains.append(temp)
+        domainNames.append(i)
 
-domainNames = []
-domains = []
-for i in names:
-    t = (i,domain[i])
-    temp = i + " = Variable" +  str(t)
-    domains.append(temp)
-    domainNames.append(i)
+    factorNames = []
+    factors = []
+    for i in names:
+        probability = '['+','.join(probTable[i])+']'
+        given = '['+','.join(givenTable[i])+']'
+        factor = "f_" + i + " = Prob(" + i + "," + given +"," + probability + ")"
+        factors.append(factor)
+        factorNames.append("f_" + i)
 
-factorNames = []
-factors = []
-for i in names:
-    probability = '['+','.join(probTable[i])+']'
-    given = '['+','.join(givenTable[i])+']'
-    factor = "f_" + i + " = Prob(" + i + "," + given +"," + probability + ")"
-    factors.append(factor)
-    factorNames.append("f_" + i)
-
-template = '\n'.join(domains) + " \n"+ "\n".join(factors) + "\n" +probname + """ = Belief_network(
-    vars=[$vars],
-    factors=[$probs],
-    positions=$positions)"""
+    template = '\n'.join(domains) + " \n"+ "\n".join(factors) + "\n" +probname + """ = Belief_network(
+        vars=[$vars],
+        factors=[$probs],
+        positions=$positions)"""
 
 
 
 
-print( Template(template).substitute(
-        vars= ','.join(domainNames),
-        probs=',' .join(factorNames),
-        positions=[]))
-
+    print( Template(template).substitute(
+            vars= ','.join(domainNames),
+            probs=',' .join(factorNames),
+            positions=[]))
--- problemParsers/searchParser.py
+++ problemParsers/searchParser.py
@@ -7,59 +7,63 @@ Created on Tue Aug 13 20:13:50 2019
 """
 
 import xml.etree.ElementTree as ET
-tree = ET.parse('/Users/zijiazhang/sample.xml')
-root = tree.getroot()
-graph = root[0]
+def loadFromXML(path):
+    try:
+        tree = ET.parse(path)
+    except:
+        print('Error Reading Files')
+        return
+    root = tree.getroot()
+    graph = root[0]
 
-result = 'Search_problem_from_explicit_graph(';
-nodes = set()
-nodemap = {}
-start = '';
-goal = set();
-hmap = {}
-Edges = []
-positions = {}
+    result = 'Search_problem_from_explicit_graph(';
+    nodes = set()
+    nodemap = {}
+    start = '';
+    goal = set();
+    hmap = {}
+    Edges = []
+    positions = {}
 
-for element in graph:
-    if(element.tag == 'NODE'):
-        name = element.find('NAME').text
-        index = element.find('INDEX').text
-        nodetype = element.find('TYPE').text
-        heuristic = float(element.find('HEURISTICVALUE').text)
-        xpos = float(element.find('XPOS').text)
-        ypos = float(element.find('YPOS').text)
-        positions[name] = (xpos,ypos)
-        nodes.add(name);
-        nodemap[index] = name;
-        hmap[name] = heuristic
-        if(nodetype == 'START'):
-            start = name
-        elif(nodetype == 'GOAL'):
-            goal.add(name)
-    if(element.tag == 'EDGE'):
-        startIndex = element.find('STARTINDEX').text
-        endIndex = element.find('ENDINDEX').text
-        cost = float(element.find('COST').text)
-        temp = 'Arc'
-        temp+= str((nodemap[startIndex],nodemap[endIndex],cost))
-        Edges.append(temp)
+    for element in graph:
+        if(element.tag == 'NODE'):
+            name = element.find('NAME').text
+            index = element.find('INDEX').text
+            nodetype = element.find('TYPE').text
+            heuristic = float(element.find('HEURISTICVALUE').text)
+            xpos = float(element.find('XPOS').text)
+            ypos = float(element.find('YPOS').text)
+            positions[name] = (xpos,ypos)
+            nodes.add(name);
+            nodemap[index] = name;
+            hmap[name] = heuristic
+            if(nodetype == 'START'):
+                start = name
+            elif(nodetype == 'GOAL'):
+                goal.add(name)
+        if(element.tag == 'EDGE'):
+            startIndex = element.find('STARTINDEX').text
+            endIndex = element.find('ENDINDEX').text
+            cost = float(element.find('COST').text)
+            temp = 'Arc'
+            temp+= str((nodemap[startIndex],nodemap[endIndex],cost))
+            Edges.append(temp)
 
 
-from string import Template
+    from string import Template
 
-template = """Search_problem_from_explicit_graph(
-    nodes=$nodes,
-    arcs=[$arcs],
-    start=$start,
-    goals=$goals,
-    hmap=$hmap,
-    positions=$positions)"""
+    template = """Search_problem_from_explicit_graph(
+        nodes=$nodes,
+        arcs=[$arcs],
+        start=$start,
+        goals=$goals,
+        hmap=$hmap,
+        positions=$positions)"""
 
-print(Template(template).substitute(
-        nodes=nodes,
-        arcs=', '.join(Edges),
-        start=start.__repr__(),
-        goals=goal,
-        hmap=hmap,
-        positions = positions))
-#print(result + str(nodes) + str(start) + str(goal) + str(hmap))
+    print(Template(template).substitute(
+            nodes=nodes,
+            arcs=', '.join(Edges),
+            start=start.__repr__(),
+            goals=goal,
+            hmap=hmap,
+            positions = positions))
