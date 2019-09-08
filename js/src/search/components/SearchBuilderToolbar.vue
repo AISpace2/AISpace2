41707ca0f9f0d5722553ee894d65c44e837034e7
LiuJingru <d_soushiki@LiuJingrus-MacBook-Pro.local>
2019-09-08 01:19:38 -0700

    improved search_builder
    added separation of overlapped edges, improved search builder.

---
+++ js/src/search/components/SearchBuilderToolbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <span>
+    <span class="radioInputGroup">
+      <input type="radio" id="zero" value="create" v-model="picked" />
+      <label for="zero">Create</label>
+    </span>
+    <span class="radioInputGroup">
+      <input type="radio" id="one" value="select" v-model="picked" />
+      <label for="one">Select</label>
+    </span>
+    <span class="radioInputGroup">
+      <input type="radio" id="two" value="delete" v-model="picked" />
+      <label for="two">Delete</label>
+    </span>
+  </span>
+</template>
+
+<script lang="ts">
+import Vue, { ComponentOptions } from "vue";
+import Component from "vue-class-component";
+import { Prop, Watch } from "vue-property-decorator";
+
+type Mode = "create" | "select" | "delete";
+
+/**
+ * Toolbar to switch between modes while creating a CSP.
+ */
+@Component
+export default class SearchToolbar extends Vue {
+  picked: Mode = "create";
+
+  @Watch("picked")
+  onPickedChanged() {
+    this.$emit("modechanged", this.picked);
+  }
+}
+</script>
+
+<style scoped>
+</style>
