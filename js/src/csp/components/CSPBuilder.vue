84967cca065ea5c90fc947a66c121a13c9855f8c
LiuJingru <d_soushiki@LiuJingrus-MacBook-Pro.local>
2019-09-12 17:20:47 -0700

    minor change

--- js/src/csp/components/CSPBuilder.vue
+++ js/src/csp/components/CSPBuilder.vue
@@ -306,7 +306,7 @@
       </p>
       <div :class="getDeletionConfirmationClass(to_delete)">
         <strong>Confirm Deletion?</strong>
-        <button ref="delete_yes" @click="deleteSelection()">Yes</button>
+        <button ref="delete_yes" @click="deleteSelection()" @blur="selection = null">Yes</button>
         <button ref="delete_no" @click="selection = null, to_delete = false">No</button>
       </div>
       <span class="successText">{{succeed_message}}</span>
