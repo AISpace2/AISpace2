38293744294b7804e206c37885959ce833df5b43
Yifei Chen <yifeichen@163.com>
2020-05-21 00:25:59 -0700

    Update SearchUtils.ts

--- js/src/search/SearchUtils.ts
+++ js/src/search/SearchUtils.ts
@@ -4,7 +4,7 @@ import * as labelDict from "../labelDictionary";
 /** Returns a formatted string representing the search node's heuristic value. */
 export function nodeHText(node: ISearchGraphNode) {
   // Ensures proper rounding for e.g. 8.45 -> 8.5, which fails just using toFixed (8.4)
-  return "heur: " + (Math.round(10 * node.h) / 10).toFixed(1);
+  return "heur: " + (Math.round(10 * node.heuristic) / 10).toFixed(1);
 }
 
 /** Returns the fill colour of a search node based on its type. */
