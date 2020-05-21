a72997a7f012802dfd4e73ce46a810cf1b7b8269
Yifei Chen <yifeichen@163.com>
2020-05-20 23:31:51 -0700

    Update DirectedRectEdge.vue
    shortened the runtime of adjustedX2() and adjustedY2() by a new function adjustedEnd()

--- js/src/components/DirectedRectEdge.vue
+++ js/src/components/DirectedRectEdge.vue
@@ -27,16 +27,18 @@
 
     /** The x-coordinate to place the end of the path. It is adjusted to be on the edge of the node. */
     get adjustedX2() {
-      let point = this.intersectPoint();
-      return point ? point.x : this.x2;
+      // let point = this.intersectPoint();
+      // return point ? point.x : this.x2;
       // return this.x2;
+      return this.adjustedEnd().x;
     }
 
     /** The y-coordinate to place the end of the path. It is adjusted to be on the edge of the node. */
     get adjustedY2() {
-      let point = this.intersectPoint();
-      return point ? point.y : this.y2;
+      // let point = this.intersectPoint();
+      // return point ? point.y : this.y2;
       // return this.y2;
+      return this.adjustedEnd().y;
     }
 
     /* Slope of the edge */
@@ -64,43 +66,78 @@
       return {x: (dy-this.intercept())/this.slope(), y: dy};
     }
 
-    // Find the x,y coordinates of the arrow head i.e. the destination coordinates of a directed edge.
-    intersectPoint() : {x: number, y:number} | null {
+    // // Find the x,y coordinates of the arrow head i.e. the destination coordinates of a directed edge.
+    // intersectPoint() : {x: number, y:number} | null {
 
+    //   // First make four linear test lines (y = mx + b) along the four boundaries of the target box.
+    //   const xLeft = this.x2 - this.graph_node_width/2;
+    //   const xRight = this.x2 + this.graph_node_width/2;
+    //   const yUp = this.y2 + this.graph_node_height/2;
+    //   const yDown = this.y2 - this.graph_node_height/2;
+
+    //   // setting the valid range for x and y (the range between the two graph nodes for the edge)
+    //   const xBound = this.x1 < this.x2
+    //     ? {lower: this.x1, upper: this.x2} : {lower: this.x2, upper: this.x1};
+    //   const yBound = this.y1 < this.y2
+    //     ? {lower: this.y1, upper: this.y2} : {lower: this.y2, upper: this.y1};
+
+    //   // Find the intersection point of the directed rect edge and the test lines.
+    //   // Find the one intersection point, (ix, iy), such that x1 <= ix <= x2 an y1 <= iy <= y2.
+    //   const intersectionPoint =  [
+    //     this.intersectX(xLeft),
+    //     this.intersectX(xRight),
+    //     this.intersectY(yUp),
+    //     this.intersectY(yDown)
+    //   ].reduce((acc, point)=>{
+    //     if(point.x >= xLeft && point.x <= xRight && point.y <= yUp && point.y >= yDown){
+    //       if(point.x >= xBound.lower && point.x <= xBound.upper){
+    //         acc = point;
+    //       } else if(point.y >= yBound.lower && point.y <= yBound.upper) {
+    //         acc = point;
+    //       }
+    //     }
+    //     return acc;
+    //   }, {x: -1, y: -1});
+
+    //   if (intersectionPoint.x != -1 && intersectionPoint.y != -1)
+    //     return intersectionPoint;
+    //   else
+    //     return null;
+    // }
+
+    /** The x-coordinate and y-coordinate to place the start of the path. It is adjusted to be on the edge of the node. */
+    adjustedStart() {
+
+    }
+
+    /** The x-coordinate and y-coordinate to place the end of the path. It is adjusted to be on the edge of the node. */
+    adjustedEnd() {
       // First make four linear test lines (y = mx + b) along the four boundaries of the target box.
       const xLeft = this.x2 - this.graph_node_width/2;
       const xRight = this.x2 + this.graph_node_width/2;
-      const yUp = this.y2 + this.graph_node_height/2;
-      const yDown = this.y2 - this.graph_node_height/2;
+      const yUp = this.y2 - this.graph_node_height/2;
+      const yDown = this.y2 + this.graph_node_height/2;
 
-      // setting the valid range for x and y (the range between the two graph nodes for the edge)
-      const xBound = this.x1 < this.x2
-        ? {lower: this.x1, upper: this.x2} : {lower: this.x2, upper: this.x1};
-      const yBound = this.y1 < this.y2
-        ? {lower: this.y1, upper: this.y2} : {lower: this.y2, upper: this.y1};
+      // setting the cut lines for x and y 
+      const xBound = this.x1 < this.x2 ? xLeft : xRight;
+      const yBound = this.y1 < this.y2 ? yUp : yDown;
 
-      // Find the intersection point of the directed rect edge and the test lines.
-      // Find the one intersection point, (ix, iy), such that x1 <= ix <= x2 an y1 <= iy <= y2.
-      const intersectionPoint =  [
-        this.intersectX(xLeft),
-        this.intersectX(xRight),
-        this.intersectY(yUp),
-        this.intersectY(yDown)
-      ].reduce((acc, point)=>{
-        if(point.x >= xLeft && point.x <= xRight && point.y <= yUp && point.y >= yDown){
-          if(point.x >= xBound.lower && point.x <= xBound.upper){
-            acc = point;
-          } else if(point.y >= yBound.lower && point.y <= yBound.upper) {
-            acc = point;
-          }
-        }
-        return acc;
-      }, {x: -1, y: -1});
 
-      if (intersectionPoint.x != -1 && intersectionPoint.y != -1)
-        return intersectionPoint;
-      else
-        return null;
+      const cutByXBoundPoint = this.intersectX(xBound);
+      const cutByYBoundPoint = this.intersectY(yBound);
+
+
+      const cutByXBoundNewY = cutByXBoundPoint.y;
+      const cutByYBoundNewX = cutByYBoundPoint.x;
+
+
+      //return the pair of x-coordinate and y-coordinate that touches the node
+      if(cutByXBoundNewY<yUp || cutByXBoundNewY>yDown){
+        return cutByYBoundPoint;
+      } else {
+        return cutByXBoundPoint;
+      }
+      
     }
   }
 
