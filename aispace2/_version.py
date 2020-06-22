1346eb74dcfc7cd7eef6a28917e155936b5f9a78
Yifei Chen <yifeichen@163.com>
2020-06-22 16:30:57 -0700

    Update _version.py

--- aispace2/_version.py
+++ aispace2/_version.py
@@ -1,5 +1,5 @@
 # Do not change this manually. Change this by runnning /updateVersion.py
-version_info = (0, 8, 1)
+version_info = (0, 8, 2)
 __version__ = '.'.join(map(str, version_info))
 
 update_notification = True
